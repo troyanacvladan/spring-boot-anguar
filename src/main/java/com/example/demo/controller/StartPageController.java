0dc974e483e145fa5ded4869743c309bce0fa9f7
vladan.jovanovic <vladan.jovanovic@prozone.rs>
2020-08-03 13:09:37 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/controller/StartPageController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StartPageController {
+
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/")
+   // @ResponseBody
+    String index(){
+        logger.info("Method started");
+        return "Welcome to Spring Boot app!";
+    }
+}
